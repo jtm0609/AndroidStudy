7b1b1b265838526e895602a342746f76c048a4bb
Takmin <jtm0609@naver.com>
2020-03-19 15:44:45 +0900

    깡샘 안드로이드 실습 : 리소스의 전반적인 이해(drawble, layout, values, anim, mipmap)

---
+++ AndroidStudy/app/src/main/java/com/jtmcompany/androidstudy/Part2_7_Resource.java
@@ -0,0 +1,43 @@
+package com.jtmcompany.androidstudy;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Part2_7_Resource extends AppCompatActivity {
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_part2_7__resource);
+        imageView=findViewById(R.id.img);
+        //에니메이션 획득
+        Animation anim= AnimationUtils.loadAnimation(this,R.anim.in);
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Animation anim=AnimationUtils.loadAnimation(Part2_7_Resource.this,R.anim.move);
+
+                //마지막상황에서 안멈추게하기
+                //안그러면 원상복귀가됨
+                anim.setFillAfter(true);
+                imageView.startAnimation(anim);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        imageView.startAnimation(anim);
+    }
+}
---
+++ AndroidStudy/app/src/main/java/com/jtmcompany/androidstudy/Part2_7_Resource2.java
@@ -0,0 +1,14 @@
+package com.jtmcompany.androidstudy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Part2_7_Resource2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_part2_7__resource2);
+    }
+}
